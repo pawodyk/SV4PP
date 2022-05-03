577eadb6219b2ac14f3e6e91b8e6796a99623110
pwodyk <14218944+pawodyk@users.noreply.github.com>
2022-05-03 23:18:24 +0100

    retrived results from bandit in json foramt

--- SV4PP/__main__.py
+++ SV4PP/__main__.py
@@ -1,6 +1,7 @@
 import click
 import bandit
 import subprocess
+import json
 
 
 @click.command()
@@ -10,9 +11,16 @@ def runCLI():
     file = "test.py" # temporary hardcoded location
 
 
-    bandit = subprocess.Popen(["bandit", "-h"], stdout=subprocess.PIPE)
-    print(bandit.communicate())
+    bandit = subprocess.Popen(["bandit", "-f", "json", file, "-q"], stdout=subprocess.PIPE)
+    output = bandit.communicate()
+    output_json = json.loads(output[0].decode('utf8'))
+    # print(type(output_json))
 
+    # for x, y in output_json.items() : print(x, y)
+    # print(output_json['results'])
+
+    for x in output_json['results'] :
+        for y, z in x.items() : print(y, z)
 
 
 if __name__ == '__main__':
