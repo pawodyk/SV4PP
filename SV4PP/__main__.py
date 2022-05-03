b3fe73316b3764b9c2817530fd542c6e7d96cea1
pwodyk <14218944+pawodyk@users.noreply.github.com>
2022-05-03 22:21:18 +0100

    added process for bandit

--- SV4PP/__main__.py
+++ SV4PP/__main__.py
@@ -1,9 +1,17 @@
 import click
+import bandit
+import subprocess
+
 
 @click.command()
 def runCLI():
-    print(__name__)
+    # print(__name__)
 
+    file = "test.py" # temporary hardcoded location
+
+
+    bandit = subprocess.Popen(["bandit", "-h"], stdout=subprocess.PIPE)
+    print(bandit.communicate())
 
 
 
